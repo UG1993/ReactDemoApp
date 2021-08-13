3e0cdb97e14123d2269f53b8176c9d16e5c8f934
UG1993 <86815115+UG1993@users.noreply.github.com>
2021-08-13 23:26:09 +0530

    Add

---
+++ App.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import Routers from './src';
+
+const App = () => {
+  return (
+    <Routers />
+  );
+};
+
+export default App;
